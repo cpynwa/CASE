4fe78567802f47996e1bb0fec3e8106f2916f3df
cpynwa <cpynwa@gmail.com>
2022-01-23 05:06:32 +0000

    passwd change

--- account/views.py
+++ account/views.py
@@ -52,7 +52,7 @@ def change_password(request):
             user = form.save()
             update_session_auth_hash(request, user)  # Important!
             messages.success(request, 'Your password was successfully updated!')
-            return redirect('/login')
+            return redirect('change_password')
         else:
             messages.error(request, 'Please correct the error below.')
     else:
